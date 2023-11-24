715e41a701fe661e125745c1672251c07491350e
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-11-24 22:37:44 +0200

    + exchange rate & converter

--- index.js
+++ index.js
@@ -1,58 +1,96 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+// BACKGROUND & PHOTOGRAFER
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.user);
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById(
+      "author"
+    ).textContent = `By: ${data.user.name}, ${data.user.location}`;
+  })
+  .catch((err) => {
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1494564605686-2e931f77a8e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDA4NTI5NjN8&ixlib=rb-4.0.3&q=80&w=1080&quot;)`;
+    document.getElementById("author").textContent = `By: Jeremy Bishop, California`;
+  });
+
+
+
+
+// RATE EXCHANGE
+const amount = document.getElementById("rate-amount");
+const selectFrom = document.getElementById("rate-select-from");
+const selectTo = document.getElementById("rate-select-to");
+const result = document.getElementById("rate-exchange-result");
+const exchangeBtn = document.getElementById("rate-equal-btn");
+const moneyCodes = ["USD", "EUR"];
+
+moneyCodes.forEach((code) => {
+  fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/${code}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Something went wrong");
+      }
+      return res.json();
     })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
+    .then((data) => {
+      document.getElementById("rate-top").innerHTML += `
+        <p>${data.base_code} = ${(data.conversion_rates.UAH).toFixed(2)} UAH</p>
+      `;
     })
-    .catch(err => console.error(err))
+    .catch((err) => console.error(err));
+});
+
+exchangeBtn.addEventListener("click", () => {
+  if (amount.value) {
+    fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/USD`)
+      .then(res => res.json())
+      .then(data => {
+        const fromExchangeRate = data.conversion_rates[selectFrom.value];
+        const toExchangeRate = data.conversion_rates[selectTo.value];
+        const convertedAmount = (amount.value / fromExchangeRate) * toExchangeRate;
+
+        result.textContent = `${(convertedAmount).toFixed(2)} ${selectTo.value}`
+      })
+  } else {
+    alert("Add amount!")
+  }
+})
+
+
+
+// TIME IN CENTER
 
 function getCurrentTime() {
-    const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+  const date = new Date();
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-us",
+    { timeStyle: "short" }
+  );
 }
 
-setInterval(getCurrentTime, 1000)
+setInterval(getCurrentTime, 1000);
 
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            document.getElementById("weather").innerHTML = `
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
                 <p class="weather-city">${data.name}</p>
-            `
-        })
-        .catch(err => console.error(err))
+            `;
+    })
+    .catch((err) => console.error(err));
 });

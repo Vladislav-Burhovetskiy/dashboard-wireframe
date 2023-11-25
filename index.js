27ec784660095734d373ea627c3941257b6402c6
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-11-26 00:11:38 +0200

    Add js for search

--- index.js
+++ index.js
@@ -94,3 +94,19 @@ navigator.geolocation.getCurrentPosition((position) => {
     })
     .catch((err) => console.error(err));
 });
+
+// GOOGLE SEARCH
+const form = document.getElementById("search-form")
+const query = document.getElementById("search-query")
+const google = "https://www.google.com/search?q=";
+
+form.addEventListener("submit", search)
+
+function search(e) {
+  e.preventDefault();
+
+  if (query.value) {
+    const url = google + query.value;
+    const win = window.open(url, '_blank');
+  }
+}

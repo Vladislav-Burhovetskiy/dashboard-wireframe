64afc21bad2ad071df9a8965d01070e4b6d150a8
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-11-26 23:39:16 +0200

    Add TO DO section html css js

--- index.js
+++ index.js
@@ -28,37 +28,37 @@ const result = document.getElementById("rate-exchange-result");
 const exchangeBtn = document.getElementById("rate-equal-btn");
 const moneyCodes = ["USD", "EUR"];
 
-moneyCodes.forEach((code) => {
-  fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/${code}`)
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Something went wrong");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      document.getElementById("rate-top").innerHTML += `
-        <p>${data.base_code} = ${(data.conversion_rates.UAH).toFixed(2)} UAH</p>
-      `;
-    })
-    .catch((err) => console.error(err));
-});
+// moneyCodes.forEach((code) => {
+//   fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/${code}`)
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw Error("Something went wrong");
+//       }
+//       return res.json();
+//     })
+//     .then((data) => {
+//       document.getElementById("rate-top").innerHTML += `
+//         <p>${data.base_code} = ${(data.conversion_rates.UAH).toFixed(2)} UAH</p>
+//       `;
+//     })
+//     .catch((err) => console.error(err));
+// });
 
-exchangeBtn.addEventListener("click", () => {
-  if (amount.value) {
-    fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/USD`)
-      .then(res => res.json())
-      .then(data => {
-        const fromExchangeRate = data.conversion_rates[selectFrom.value];
-        const toExchangeRate = data.conversion_rates[selectTo.value];
-        const convertedAmount = (amount.value / fromExchangeRate) * toExchangeRate;
+// exchangeBtn.addEventListener("click", () => {
+//   if (amount.value) {
+//     fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/USD`)
+//       .then(res => res.json())
+//       .then(data => {
+//         const fromExchangeRate = data.conversion_rates[selectFrom.value];
+//         const toExchangeRate = data.conversion_rates[selectTo.value];
+//         const convertedAmount = (amount.value / fromExchangeRate) * toExchangeRate;
 
-        result.textContent = `${(convertedAmount).toFixed(2)} ${selectTo.value}`
-      })
-  } else {
-    alert("Add amount!")
-  }
-})
+//         result.textContent = `${(convertedAmount).toFixed(2)} ${selectTo.value}`
+//       })
+//   } else {
+//     alert("Add amount!")
+//   }
+// })
 
 
 
@@ -110,3 +110,25 @@ function search(e) {
     const win = window.open(url, '_blank');
   }
 }
+
+
+// TO DO
+const textBtn = document.querySelector(".text-open-btn")
+const addBtn = document.querySelector(".text-add-btn")
+const textList = document.querySelector(".text-list")
+const textBox = document.querySelector(".text-box")
+const text = document.querySelector(".text")
+
+textBtn.addEventListener("click", toggle)
+
+function toggle() {
+  text.classList.toggle("open");
+  addBtn.style.display = text.classList.contains("open") ? "block" : "none";
+};
+
+addBtn.addEventListener("click", () => {
+  if (textBox.value) {
+    textList.innerHTML += `<li>${textBox.value}</li>`
+    textBox.value = '';
+  }
+})

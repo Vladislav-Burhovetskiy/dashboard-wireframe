7947d71e84b8d351dc2bd49fdc2b8b18acbdb039
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-11-28 13:45:26 +0200

    Change new free rate API

--- scripts/rate.js
+++ scripts/rate.js
@@ -3,36 +3,46 @@ const selectFrom = document.getElementById("rate-select-from");
 const selectTo = document.getElementById("rate-select-to");
 const result = document.getElementById("rate-exchange-result");
 const exchangeBtn = document.getElementById("rate-equal-btn");
-const moneyCodes = ["USD", "EUR"];
+const moneyCodes = ["usd", "eur"];
 
-// moneyCodes.forEach((code) => {
-//   fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/${code}`)
-//     .then((res) => {
-//       if (!res.ok) {
-//         throw Error("Something went wrong");
-//       }
-//       return res.json();
-//     })
-//     .then((data) => {
-//       document.getElementById("rate-top").innerHTML += `
-//         <p>${data.base_code} = ${(data.conversion_rates.UAH).toFixed(2)} UAH</p>
-//       `;
-//     })
-//     .catch((err) => console.error(err));
-// });
+moneyCodes.forEach((code) => {
+  fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${code}.json`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Something went wrong");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const uahRate = data.eur ? data.eur.uah : data.usd.uah;
+      const currency = data.usd ? "USD" : "EUR";
 
-// exchangeBtn.addEventListener("click", () => {
-//   if (amount.value) {
-//     fetch(`https://v6.exchangerate-api.com/v6/67db86d6976ed8dfc7ac2009/latest/USD`)
-//       .then(res => res.json())
-//       .then(data => {
-//         const fromExchangeRate = data.conversion_rates[selectFrom.value];
-//         const toExchangeRate = data.conversion_rates[selectTo.value];
-//         const convertedAmount = (amount.value / fromExchangeRate) * toExchangeRate;
+      document.getElementById("rate-top").innerHTML += `
+        <p>${currency} = ${uahRate.toFixed(2)} UAH</p>
+      `;
+    })
+    .catch((err) => console.error(err));
+});
 
-//         result.textContent = `${(convertedAmount).toFixed(2)} ${selectTo.value}`
-//       })
-//   } else {
-//     alert("Add amount!")
-//   }
-// })
+exchangeBtn.addEventListener("click", () => {
+  if (amount.value) {
+    const fromValue = selectFrom.value.toLowerCase();
+    const toValue = selectTo.value.toLowerCase();
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromValue}.json`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const fromExchangeRate = data[fromValue];
+        const toExchangeRate = fromExchangeRate[toValue];
+        const convertedAmount =
+          (amount.value / fromExchangeRate[fromValue]) * toExchangeRate;
+
+        result.textContent = `${convertedAmount.toFixed(2)} ${selectTo.value}`;
+      });
+  } else {
+    alert("Add amount!");
+  }
+});

dc4d5aa3d12654f85f4514e9969485684f49913e
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-12-02 21:31:47 +0200

    delete search and time and put it to index js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-import "./scripts/photo";
-import "./scripts/rate";
-import "./scripts/weather";
-import "./scripts/time";
-import "./scripts/search";
-import "./scripts/todo";
+const form = document.getElementById("search-form");
+const query = document.getElementById("search-query");
+const google = "https://www.google.com/search?q=";
+
+form.addEventListener("submit", search);
+
+function search(e) {
+  e.preventDefault();
+
+  if (query.value) {
+    const url = google + query.value;
+    const win = window.open(url, "_blank");
+  }
+}
+
+function getCurrentTime() {
+  const date = new Date();
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-us",
+    { timeStyle: "short" }
+  );
+}
+
+setInterval(getCurrentTime, 1000);
--- scripts/time.js
+++
@@ -1,9 +0,0 @@
-function getCurrentTime() {
-  const date = new Date();
-  document.getElementById("time").textContent = date.toLocaleTimeString(
-    "en-us",
-    { timeStyle: "short" }
-  );
-}
-
-setInterval(getCurrentTime, 1000);

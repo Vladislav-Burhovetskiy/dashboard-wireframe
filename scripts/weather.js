38ac611649a629dd7324a4f5f597e696aaad4171
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-12-09 18:21:59 +0200

    update weather city name from data API

--- scripts/weather.js
+++ scripts/weather.js
@@ -13,8 +13,9 @@ navigator.geolocation.getCurrentPosition((position) => {
       document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">Dobropillia</p>
-            `;
+                <p class="weather-city">${data.name}</p>
+                `;
+                // <p class="weather-city">Dobropillia</p>
     })
     .catch((err) => console.error(err));
 });
